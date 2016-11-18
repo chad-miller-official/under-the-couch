e224579c42eb5a0bea00465f2220ca736280ff92
Chad Miller <chad.miller.3333@gmail.com>
2016-11-17 19:34:14 -0500

    Fixed 'send_to_mailing_list not defined' error

--- blog/js/write_blog_post.js
+++ blog/js/write_blog_post.js
@@ -47,7 +47,7 @@ function create_blog_post( data )
         {
             var created_blog_post = response['blog_post'];
 
-            if( send_to_mailing_list && !response['sent_email_success'] )
+            if( data['send_to_mailing_list'] && !response['sent_email_success'] )
                 alert( 'Blog post successful, but email was not sent - please contact support.' );
 
             window.location = '/blog/blog_post.php?id=' + created_blog_post;
