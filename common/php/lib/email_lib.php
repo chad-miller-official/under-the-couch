a02241245e5c924c44e18ea8122eb47d7be6a2a3
Chad Miller <chad.miller.3333@gmail.com>
2017-01-04 14:07:43 +0900

    Fixing email_lib

--- common/php/lib/email_lib.php
+++ common/php/lib/email_lib.php
@@ -9,11 +9,6 @@
         if( !$in_production )
             $to = SessionLib::get( 'user_member.gatech_email_address' );
 
-<<<<<<< Updated upstream
-=======
-        $to = 'nbond7@gatech.edu';
-
->>>>>>> Stashed changes
         $message = Swift_Message::newInstance()
             ->setSubject( $subject )
             ->setFrom( $from )
@@ -35,31 +30,6 @@
 
     function send_html_email( $to, $from, $subject, $body )
     {
-<<<<<<< Updated upstream
         return __send_email( $to, $from, $subject, $body, [ 'mime_type' => 'text/html' ] );
-=======
-        $transport = Swift_SendmailTransport::newInstance( '/usr/sbin/sendmail -bs' );
-        $mailer    = Swift_Mailer::newInstance( $transport );
-
-        $in_production = getenv( 'HTTP_PRODUCTION_ENVIRONMENT' );
-
-        if( !$in_production )
-            $to = SessionLib::get( 'user_member.gatech_email_address' );
-
-        $to = 'nbond7@gatech.edu';
-
-        $message = Swift_Message::newInstance()
-            ->setSubject( $subject )
-            ->setFrom( $from )
-            ->setTo( $to )
-            ->setBody( $body, 'text/html' );
-
-        $sent_count = $mailer->send( $message );
-
-        if( is_scalar( $to ) )
-            return $sent_count > 0;
-        else
-            return $sent_count == count( $to );
->>>>>>> Stashed changes
     }
 ?>
